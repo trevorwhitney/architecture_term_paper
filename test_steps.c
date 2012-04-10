d1dcf6bb79919bac2d67f9de34adf9ca26a7cf13
Trevor Whitney <trevorjwhitney@gmail.com>
2012-04-10 14:29:11 -0600

    changed reset_array to zero_array only, no malloc

--- test_steps.c
+++ test_steps.c
@@ -1,6 +1,6 @@
 #include "my_dgemm.h"
 
-void reset_array(int n, double *c);
+void zero_array(int n, double *c);
 
 int main (int argc, int *argv[]) {
   //matrices
@@ -22,8 +22,8 @@ int main (int argc, int *argv[]) {
     size = i * 500;
     a = (double*) malloc(size*size*sizeof(double));
     b = (double*) malloc(size*size*sizeof(double));
-    reset_array(size, c);
-    reset_array(size, atlas_c);
+    c = (double*) malloc(size*size*sizeof(double));
+    atlas_c = (double*) malloc(size*size*sizeof(double));
 
     for (j = 0; j < size * size; j++) {
       a[j] = (j % 64)*1.0;
@@ -42,25 +42,25 @@ int main (int argc, int *argv[]) {
     elapsed_time = my_dgemm(size, a, b, c);
     error = calculate_error(c, atlas_c, size);
     fprintf(results, "my_dgemm, %d, %d, %.2f, %.20f\n", size, t, elapsed_time, error);
-    reset_array(size, c);
+    zero_array(size, c);
 
     //step01
     elapsed_time = step01(size, a, b, c, t);
     error = calculate_error(c, atlas_c, size);
     fprintf(results, "step01, %d, %d, %.2f, %.20f\n", size, t, elapsed_time, error);
-    reset_array(size, c);
+    zero_array(size, c);
 
     //step02
     elapsed_time = step02(size, a, b, c, t);
     error = calculate_error(c, atlas_c, size);
     fprintf(results, "step02, %d, %d, %.2f, %.20f\n", size, t, elapsed_time, error);
-    reset_array(size, c);
+    zero_array(size, c);
 
     //step03
     elapsed_time = step03(size, a, b, c, t);
     error = calculate_error(c, atlas_c, size);
     fprintf(results, "step03, %d, %d, %.2f, %.20f\n", size, t, elapsed_time, error);
-    reset_array(size, c);
+    zero_array(size, c);
 
     //step04
     elapsed_time = step04(size, a, b, c, t);
@@ -74,10 +74,8 @@ int main (int argc, int *argv[]) {
   return 0;
 }
 
-void reset_array(int n, double *c) {
+void zero_array(int n, double *c) {
   int i, j;
-
-  c = (double*) malloc(n*n*sizeof(double));
 
   for (i = 0; i < n; i++)
     for (j = 0; j < n; j++)
