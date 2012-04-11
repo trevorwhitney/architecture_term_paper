e2bf968c25c4e0a791331f154e59b2f2bd070002
Trevor Whitney <trevorjwhitney@gmail.com>
2012-04-10 22:09:56 -0600

    step04

--- my_dgemm.h
+++ my_dgemm.h
@@ -150,7 +150,6 @@ double step04(int n, double *a, double *b, double *c, int t) {
   register double r;
 
   start = clock();
-  extra = MIN(t, n) % 2;
   for (jj = 0; jj < n; jj += t) {
     for (kk = 0; kk < n; kk += t) {
       for (ii = 0; ii < n; ii += t) {
@@ -158,9 +157,14 @@ double step04(int n, double *a, double *b, double *c, int t) {
           nxj = n*j;
           for (i = ii; i < MIN(ii+t, n); i++) {
             r = c[nxj+i];
-            for (k = kk; k < MIN(kk+t, n)-2; k+=2) {
+            for (k = kk; k < MIN(kk+t, n)-7; k+=7) {
               r = r + b[nxj+k] * a[n*k+i];
               r = r + b[nxj+k+1] * a[n*(k+1)+i];
+              r = r + b[nxj+k+2] * a[n*(k+2)+i];
+              r = r + b[nxj+k+3] * a[n*(k+3)+i];
+              r = r + b[nxj+k+4] * a[n*(k+4)+i];
+              r = r + b[nxj+k+5] * a[n*(k+5)+i];
+              r = r + b[nxj+k+6] * a[n*(k+6)+i];
             }
             for (k; k < MIN(kk+t, n); k++) {
               r = r + b[nxj+k] * a[n*k+i];
