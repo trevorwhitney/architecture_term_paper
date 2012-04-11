364840912a23b080727cf6f3666d83cc7395ce44
Trevor Whitney <trevorjwhitney@gmail.com>
2012-04-10 22:05:37 -0600

    step04

--- my_dgemm.h
+++ my_dgemm.h
@@ -150,6 +150,7 @@ double step04(int n, double *a, double *b, double *c, int t) {
   register double r;
 
   start = clock();
+  extra = MIN(t, n) % 2;
   for (jj = 0; jj < n; jj += t) {
     for (kk = 0; kk < n; kk += t) {
       for (ii = 0; ii < n; ii += t) {
@@ -157,12 +158,12 @@ double step04(int n, double *a, double *b, double *c, int t) {
           nxj = n*j;
           for (i = ii; i < MIN(ii+t, n); i++) {
             r = c[nxj+i];
-            for (k = kk; k < MIN(kk+t, n); k+=5) {
+            for (k = kk; k < MIN(kk+t, n)-2; k+=2) {
               r = r + b[nxj+k] * a[n*k+i];
               r = r + b[nxj+k+1] * a[n*(k+1)+i];
-              r = r + b[nxj+k+2] * a[n*(k+2)+i];
-              r = r + b[nxj+k+3] * a[n*(k+3)+i];
-              r = r + b[nxj+k+4] * a[n*(k+4)+i];
+            }
+            for (k; k < MIN(t, n)-extra; k++) {
+              r = r + b[nxj+k] * a[n*k+i];
             }
             c[nxj+i] = r;
           }
