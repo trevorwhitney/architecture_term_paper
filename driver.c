711ad46558d66701673ecb4642ca3d5a711ba63a
Trevor Whitney <trevorjwhitney@gmail.com>
2012-04-10 20:50:04 -0600

    upated driver code for tiling

--- driver.c
+++ driver.c
@@ -1,5 +1,7 @@
 #include "my_dgemm.h"
 
+void zero_array(int n, double *c);
+
 int main (int argc, int *argv[]) {
   //matrices
   double* a;
@@ -10,17 +12,17 @@ int main (int argc, int *argv[]) {
   int i, j, k, l, size, t;
   double elapsed_time, error;
   //FILE *my_csv, *atlas_csv;
-  FILE *results, *atlas_csv;
+  FILE *results;
 
   //my_csv = fopen("my_dgemm.csv", "a+");
-  atlas_csv = fopen("atlas.csv", "a+");
+  //atlas_csv = fopen("atlas.csv", "a+");
   results = fopen("tiles.csv", "a+");
   fprintf(results, "Iteration #, Size of Array, Size of Tiles, Time (Seconds), Error\n");
   //fprintf(my_csv, "Iteration #, Size of Array, Time (Seconds)\n");
-  fprintf(atlas_csv, "Iteration #, Size of Array, Time (Seconds)\n");
+  //fprintf(atlas_csv, "Iteration #, Size of Array, Time (Seconds)\n");
 
 
-  for (i = 1; i <= 10; i++) {
+  for (i = 1; i <= 5; i++) {
     printf("Starting iteration %d\n", i);
     size = i * 500;
     a = (double*) malloc(size*size*sizeof(double));
@@ -29,17 +31,18 @@ int main (int argc, int *argv[]) {
     atlas_c = (double*) malloc(size*size*sizeof(double));
 
     for (j = 0; j < size * size; j++) {
-      a[j] = (j % 64);
-      b[j] = (j % 64);
+      a[j] = (j % 64)*1.0;
+      b[j] = (j % 64)*1.0;
     }
 
     elapsed_time = my_cblas_dgemm(size, a, b, atlas_c);
-    fprintf(atlas_csv, "%d, %d, %.2f\n", i, size, elapsed_time);
+    //fprintf(atlas_csv, "%d, %d, %.2f\n", i, size, elapsed_time);
 
-    for(t = 7; t < 11; t++) {
+    for(t = 7; t < 25; t++) {
       elapsed_time = step01(size, a, b, c, t);
       error = calculate_error(c, atlas_c, size);
       fprintf(results, "%d, %d, %d, %.2f, %.5f\n", i, size, t, elapsed_time, error);
+      zero_array(size, c);
     }
 
     //elapsed_time = my_dgemm(size, a, b, c);
@@ -52,4 +55,12 @@ int main (int argc, int *argv[]) {
   fclose(results);
 
   return 0;
+}
+
+void zero_array(int n, double *c) {
+  int i, j;
+
+  for (i = 0; i < n; i++)
+    for (j = 0; j < n; j++)
+      c[i + n*j] = 0.0;
 }
