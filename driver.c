5c1dd43d5d09381a79cad86a37bb65fd9ac1221c
Trevor Whitney <trevorjwhitney@gmail.com>
2012-04-11 03:07:53 -0600

    only 4 iterations for tiles

--- driver.c
+++ driver.c
@@ -22,7 +22,7 @@ int main (int argc, int *argv[]) {
   //fprintf(atlas_csv, "Iteration #, Size of Array, Time (Seconds)\n");
 
 
-  for (i = 1; i <= 5; i++) {
+  for (i = 1; i < 5; i++) {
     printf("Starting iteration %d\n", i);
     size = i * 500;
     a = (double*) malloc(size*size*sizeof(double));
