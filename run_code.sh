c18822d48427980fdd8f65d1a03e732229dcc83c
Trevor Whitney <trevorjwhitney@gmail.com>
2012-04-23 14:48:48 -0600

    created script to run all code

---
+++ run_code.sh
@@ -0,0 +1,20 @@
+#! /bin/sh
+cd O0
+./final_O0
+cd ../O0_funroll
+./final_O0_funroll
+
+cd ../O1
+./final_O1
+cd ../O1_funroll
+./final_O1_funroll
+
+cd ../O2
+./final_O2
+cd ../O2_funroll
+./final_O2_funroll
+
+cd ../O3
+./final_O3
+cd ../O3_funroll
+./final_O3_funroll
